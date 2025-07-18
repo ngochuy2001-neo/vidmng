9cf5a556336206ebf1717a11c63620118eb40f18
Nguyen Le Ngoc Huy <71955356+ngochuy2001-neo@users.noreply.github.com>
2025-07-18 16:08:33 +0700

    Update environment for backend

--- vidmng_API/vidmng/settings.py
+++ vidmng_API/vidmng/settings.py
@@ -11,21 +11,26 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Khởi tạo biến môi trường
+env = environ.Env(
+    # đặt giá trị mặc định
+    DEBUG=(bool, False)
+)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+environ.Env.read_env(BASE_DIR / '.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bomo+m%4&k1j646+rabseq(=qx$aecu98n1e75ty%*c3t=v)o#'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
